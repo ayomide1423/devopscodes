6afb1527a1f3acbef0c897872b37b18f309706f6
ayomide orojo <orojo.ayomide14@gmail.com>
2022-01-23 13:53:58 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          i was changed!
+          i need serious help... fr fr!
         </p>
         <a
           className="App-link"
